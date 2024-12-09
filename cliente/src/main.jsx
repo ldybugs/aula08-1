dd537a90cf2771066df855dbdde5969a7df5aea7
ldybugs <vitoriaoliveiramedeiros2006@gmail.com>
2024-12-09 14:10:38 -0400

    ...

--- cliente/src/main.jsx
+++ cliente/src/main.jsx
@@ -11,6 +11,6 @@ createRoot(document.getElementById('root')).render(
             <Route path="/" element={<Home/>}/>
             <Route path="/registro" element={<Registrar />}/>
        </Routes>
-    </BrowserRouter>
-  </StrictMode>,
+    </BrowserRouter>,
+  </StrictMode>
 )
